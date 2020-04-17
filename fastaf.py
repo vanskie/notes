a84a996329b82d4189f56ad4e87553ae284a55a5
vanskie <vanskie@localhost.localdomain>
2020-04-18 02:37:47 +0300

    ffs

---
+++ fastaf.py
@@ -0,0 +1,34 @@
+#! /usr/bin/python3
+
+import re
+import requests
+import hashlib
+
+'''
+get raw request, find string to hash, hash string and POST it.
+'''
+
+# get request to site
+page = requests.get('http://139.59.202.58:31828/')
+# extract html as text
+pageHTML = page.text
+# find the string we need with regex
+# find >(\w any char or num) 20 of them which end with <
+matchObj = re.findall(r'>\w{20}<', pageHTML)
+print(matchObj)
+# extact the string from the matchObj list and remove ><
+# before we hex the string we need to 'utf8' encode it
+#matchStr = matchObj[0][1:-1].encode('utf-8')
+matchStr = matchObj[0][1: -1]
+print(matchStr)
+# now hex has the string
+# we need to supply the utf8 string with %s formatting
+# it was a pain in the ass, because after encoding the string it becomes b'string'
+# but that doesn't mean you can use hash.update() cause you still need to add b'%s'string
+result = hashlib.md5(b'%s' % matchStr.encode('utf8')).hexdigest()
+#result = hashlib.md5(matchStr.encode('utf-8')).hexdigest()
+print(result)
+
+# now we need to POST the result and print the response
+postit = requests.post('http://139.59.202.58:31828/', data = {'hash': result})
+print(postit.text)
